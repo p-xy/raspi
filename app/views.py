f5dd3e59ac1b61bc4cd5bf4671b79e06d8a8139e
wonderX0 <957218672@qq.com>
2017-05-09 11:24:36 +0800

    中文

--- app/views.py
+++ app/views.py
@@ -7,14 +7,12 @@ from django.contrib.auth.decorators import login_required
 import requests
 import json
 import time
-import datetime
 
+from picamera import PiCamera
 import RPi.GPIO as GPIO
 from .models import LED_FORM
 from hardware.led import LED
-from hardware.face import FACE
 from hardware import dht11
-from picamera import PiCamera
 
 
 
@@ -23,7 +21,7 @@ from picamera import PiCamera
 def index(request):
 	if request.method == 'POST':
 		face_or_camera = request.POST['face_or_camera']
-		if face_or_camera =='face':
+		if face_or_camera =='人脸比对':
 			# 你的face++的应用api_key和api_secret
 			api_key = 'vigklkgJlKAFaSOuRfQGNcNAPz2Jrkfk'
 			api_secret = 'rnLgNWHIACuE6KcpWIlxf13Bc6uDpqDW'
@@ -46,13 +44,17 @@ def index(request):
 			return render(request,'index.html',{ 'JSON':JSON,'confidence':confidence })
 			
 		else:
+			#实例化一个相机类
 			camera = PiCamera()
 			camera.start_preview()
+			#相机启动需要一定时间
 			time.sleep(5)
-			camera.capture('app/static/img/image.jpg')
+			#拍取一张照片，保存在app/static/img目录下
+			camera.capture('app/static/img/image2.jpg')
+			#关闭相机
 			camera.close()
 			
-			return render(request,'index.html')
+			return redirect(request,'index.html')
 	
 	return render(request,'index.html')
 			
@@ -61,15 +63,16 @@ def index(request):
 #查看
 @login_required()
 def look(request):
-	#LED
-	p1 = LED_FORM.objects.get(id=1)
+	#读取LED开关值
+	p1 = LED_FORM.objects.get(id=1)	#id为LED_FORM表单的外键，存储的开关值的列为switch
+	#把开关值传递给p2，以上下文方式提交到模板
 	p2 = p1.switch
 	
-	# DHT11
+	# DHT11温/湿度传感器数据
 	instance = dht11.DHT11(8)
 	result = instance.read()
 	if result.is_valid():
-		p4 ="Last valid input: " + str(datetime.datetime.now())
+		#p4 ="Last valid input: " + str(datetime.datetime.now())
         p5 = "温度: %d ℃" % result.temperature
         p6 = "湿度: %d %%" % result.humidity
         GPIO.cleanup(8)
@@ -84,11 +87,16 @@ def control(request):
 			
 	if request.method=="POST":
 		''' LED'''
+		#读取表单提交数据
 		state = request.POST.get('switch')
+		#把开关数据存储到数据库
 		p1 = LED_FORM.objects.get(id=1)
+		#switch为存储开关值的列
 		p1.switch=state
 		p1.save()
+		#实例化一个LED类
 		led = LED(40,state)
+		#LED.switch（）为LED类的开关控制方法
 		led.switch()		
 		return render(request,'control.html',{'state':state })				
 				
